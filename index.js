1e09620282e5c8e45c0fd3ed27ec8354aebf5c4e
Anthony Mugendi <ngurumugz@gmail.com>
2023-09-05 15:05:19 +0300

    added associatedWith Key
    better matches

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /**
- * Copyright (c) 2023 Anthony Mugendi
- *
- * This software is released under the MIT License.
- * https://opensource.org/licenses/MIT
- */
+* Copyright (c) 2023 Anthony Mugendi
+*
+* This software is released under the MIT License.
+* https://opensource.org/licenses/MIT
+*/
 
-const mime = require('mime-type/with-db');
+const mime = require('./lib/mime');
 const fileType = require('./lib/get-type');
 const icons = require('./lib/icons');
-const typeMatches = require('./lib/type-matches');
+const {
+    type_synonyms,
+    type_matches
+} = require('./lib/type-matches');
+
 
 class MimeIt {
     constructor() {
@@ -37,7 +41,9 @@ class MimeIt {
             }
 
             let mimeType = mime.lookup(file) || mime.glob(file)[0] || null;
-            this.meta = { file };
+            this.meta = {
+                file
+            };
 
             if (!mimeType) {
                 let type = await fileType(file);
@@ -46,7 +52,7 @@ class MimeIt {
                 this.meta.took = type.took;
 
                 this.meta.source = {
-                    type: type.isUrl ? 'URL' : type.isFile ? 'FILE' : null,
+                    type: type.isUrl ? 'URL': type.isFile ? 'FILE': null,
                     size: type.size,
                     bytesRead: type.bytesRead || 0,
                 };
@@ -56,41 +62,60 @@ class MimeIt {
                 mimeType = type.mime;
             }
 
-            let [type, subType] = mimeType.split('/');
+            let [type,
+                subType] = mimeType.split('/');
 
             const ext = /\.[a-z0-9]+$/i.test(file)
-                ? file.split('.').pop()
-                : mime.extension(mimeType);
-
-            let subTypes = typeMatches(ext)
-                .filter((s) => s !== type && s !== subType)
-                .reverse();
-
-            subType = [subType, ...subTypes];
+            ? file.split('.').pop(): mime.extension(mimeType);
 
             // console.log(typeMatches(ext));
 
-            const {
+            let {
                 extensions: all,
                 charset,
                 compressible,
-            } = mime[mimeType] || { extensions: [ext] };
+            } = mime[mimeType] || {
+                extensions: [ext]
+            };
+
+            // filter unique
+            all = Array.from(new Set(all))
+
+            let associatedWith = all.map(t=>type_matches(t)).filter(v=>v.length)
+            .concat([subType.split('.').slice(1)])
+            .concat(/^[a-z]+$/.test(subType) ? [[subType]]: [[]])
+            .reduce((a, b)=> {
+                for (let c of b) {
+                    if (a.indexOf(c) == -1) {
+                        a.push(c)
+                    }
+                }
+
+                return a
+            },
+                [])
+
+            associatedWith = type_synonyms(associatedWith,ext);
+
 
             const extensions = {
                 current: ext,
                 all,
             };
 
+            const contentType = mime.contentType(mimeType);
+
             const mimeObj = {
                 type,
                 subType,
+                associatedWith,
 
-                charset: charset || null,
-                compressible: compressible || null,
+                //compressible: compressible || null,
 
                 mime: {
                     type: mimeType,
-                    content: mime.contentType(mimeType),
+                    content: contentType,
+                    charset: charset || (contentType.match(/charset=(.+)/) || [])[1] || null,
                 },
 
                 //ext,
@@ -113,9 +138,14 @@ function get_icon(mimeObj) {
     // try to get icon given extension
     let icon = icons[mimeObj.extensions.current];
 
+    //
+    if (!icon) {
+        icon = icons[mimeObj.subType] ;
+    }
+
     // if failed, try use the subtype array
     if (!icon) {
-        for (let s of mimeObj.subType) {
+        for (let s of mimeObj.associatedWith) {
             icon = icons[s];
             if (icon) break;
         }
